f227286366030f506e1631ab9e1d8e596cfd2cd7
thuviengithub0208 <khoaphp@yahoo.com>
2018-07-10 17:59:31 +0700

    "Hoan tat"

--- app/src/main/java/com/ptp/phamtanphat/listviewnangcao1206/MainActivity.java
+++ app/src/main/java/com/ptp/phamtanphat/listviewnangcao1206/MainActivity.java
@@ -21,12 +21,15 @@ public class MainActivity extends AppCompatActivity {
         lvMonan = findViewById(R.id.listview);
 
         monanArrayList = new ArrayList<>();
+
         monanArrayList.add(new Monan("Com tron",50000,R.drawable.moncomtron));
         monanArrayList.add(new Monan("Banh gao",20000,R.drawable.monbanhgao));
         monanArrayList.add(new Monan("Kim chi",45000,R.drawable.monkimchi));
         monanArrayList.add(new Monan("Mi Han Quoc",65000,R.drawable.monmihanquoc));
         monanArrayList.add(new Monan("Sushi",30000,R.drawable.monsushi));
 
+        monanAdapter = new MonanAdapter(MainActivity.this,R.layout.dong_item_monan,monanArrayList);
+        lvMonan.setAdapter(monanAdapter);
     }
 
 }
