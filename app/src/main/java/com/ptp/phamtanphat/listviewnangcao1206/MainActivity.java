90ecb2842d6ef72f831b03d375a297ce5a1533d9
thuviengithub0208 <khoaphp@yahoo.com>
2018-07-10 17:57:05 +0700

    "Gan du lieu vao trong mang object"

--- app/src/main/java/com/ptp/phamtanphat/listviewnangcao1206/MainActivity.java
+++ app/src/main/java/com/ptp/phamtanphat/listviewnangcao1206/MainActivity.java
@@ -2,15 +2,30 @@ package com.ptp.phamtanphat.listviewnangcao1206;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    ListView lvMonan;
+    ArrayList<Monan> monanArrayList;
+    MonanAdapter monanAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lvMonan = findViewById(R.id.listview);
+
+        monanArrayList = new ArrayList<>();
+        monanArrayList.add(new Monan("Com tron",50000,R.drawable.moncomtron));
+        monanArrayList.add(new Monan("Banh gao",20000,R.drawable.monbanhgao));
+        monanArrayList.add(new Monan("Kim chi",45000,R.drawable.monkimchi));
+        monanArrayList.add(new Monan("Mi Han Quoc",65000,R.drawable.monmihanquoc));
+        monanArrayList.add(new Monan("Sushi",30000,R.drawable.monsushi));
 
     }
 
